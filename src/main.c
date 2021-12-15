5b0ff64d4fadecb00221eb9c3550c9a16113a367
Dmitry Marakasov <amdmi3@amdmi3.ru>
2021-12-15 22:43:50 +0300

    Add long options and more elaborate help (#1)

--- src/main.c
+++ src/main.c
@@ -21,28 +21,68 @@
 #include <stdio.h>
 #include <getopt.h>
 
+
+static struct option longopts[] = {
+	{ "edit", no_argument, NULL, 'e' },
+	{ "help", no_argument, NULL, 'h' },
+	{ "version", no_argument, NULL, 'v' },
+	{ NULL, 0, NULL, 0 },
+};
+
+
+void usage(const char* progname, FILE* fd) {
+	(void)progname;
+	fprintf(
+		fd,
+		/* 70 chars                                                         */
+		"Usage: %1$s [--version] [--help] [MAPFILE]\n"
+		"   or: %1$s --edit MAPFILE\n"
+		"\n"
+		"You are trapped in the CAVEZ of PHEAR, your mission is to escape\n"
+		"through all the caves and make it out alive. To escape through a\n"
+		"cave you will have to find all the diamonds located in it. Once\n"
+		"you've found all the diamonds, their powers combined will help you\n"
+		"get to the next cave, one step closer to freedom.\n"
+		"\n"
+		"Options:\n"
+		"  -h, --help     display this help\n"
+		"  -v, --version  display program version\n"
+		"  -e, --edit     run level editor on the specified file\n"
+		"\n"
+		/* 70 chars                                                         */
+		, progname
+	);
+}
+
+
 int main(int argc, char** argv) {
+	int edit_mode = 0;
 	int c;
 
-	while ((c = getopt(argc, argv, "e:vh")) != -1) {
+	while ((c = getopt_long(argc, argv, "evh", longopts, NULL)) != -1) {
 		switch (c) {
 		case 'e':
-			if (!optarg) {
-				fprintf(stderr, "usage: phear -e <file>\n");
-				return 1;
-			}
-			return editor_main(optarg);
+			edit_mode = 1;
 			break;
 		case 'v':
-			puts(VERSION);
+			printf("%s\n", VERSION);
 			return 0;
 		case 'h':
-			puts("phear [-e] [-v] [-h] [<file>]\n");
+			usage(argv[0], stdout);
 			return 0;
 		default:
+			usage(argv[0], stderr);
 			return 1;
 		}
 	}
 
-	return game_main(argv[optind]);
+	if (edit_mode) {
+		if (optind == argc) {
+			usage(argv[0], stderr);
+			return 1;
+		}
+		return editor_main(argv[optind]);
+	} else {
+		return game_main(argv[optind]);
+	}
 }
